0c3e1c02c484916cb763cb93ea314610d70ddd44
AswinPuthukaatil <aswin100396@gmail.com>
2025-02-05 14:29:44 +0530

    Defined routes and imported .pb files

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,38 @@
-// JWT validation middleware 
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Prototype-1/api-gateway-service/config"
+	"github.com/Prototype-1/api-gateway-service/internal/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			c.Abort()
+			return
+		}
+		splitToken := strings.Split(tokenString, " ")
+		if len(splitToken) != 2 || splitToken[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+			c.Abort()
+			return
+		}
+		token := splitToken[1]
+
+		claims, err := utils.ValidateJWT(token, config.JWTSecretKey)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.UserID)
+		c.Next()
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Prototype-1/api-gateway-service/internal/handlers"
+	pb "github.com/Prototype-1/api-gateway-service/proto"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to admin service: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewAdminServiceClient(conn)
+
+	router := gin.Default()
+
+
+	handlers.SetupRoutes(router, client)
+
+	serverPort := ":8080"
+	fmt.Println("API Gateway running on port", serverPort)
+	log.Fatal(router.Run(serverPort))
+}
